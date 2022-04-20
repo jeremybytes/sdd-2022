2679c1d83cb8320972f05411a6c591467c909ebd
Jeremy Clark <jeremy@jeremybytes.com>
2022-04-20 14:59:02 -0700

    Added Dependency Injection sample code

---
+++ 04-diving-deeper-into-dependency-injection/AccessModifiers/Private/ICalendarItem.cs
@@ -0,0 +1,40 @@
+﻿namespace AccessModifiers.Private;
+
+public enum CalendarItemType
+{
+    Unspecified,
+    Event,
+    Meeting,
+    Reminder,
+}
+
+public interface ICalendarItem
+{
+    public int CalendarId { get; }
+    public string Name { get; }
+    public string? Description { get; set; }
+    public DateTime StartTime { get; set; }
+
+    // Private abstract members not allowed
+    // private CalendarItemType DefaultType { get; }
+
+    // Private members must have default implementation
+    private CalendarItemType DefaultType { get => CalendarItemType.Unspecified; }
+
+    // Private members cannot be accessed outside
+    // of the interface, so they are useful only for
+    // default implementations inside the interface.
+    public CalendarItemType ItemType { get => DefaultType; }
+
+    public string ItemTypeDescription
+    {
+        get
+        {
+            return ItemType switch
+            {
+                CalendarItemType.Unspecified => ItemType.ToString(),
+                _ => $"Calendar {ItemType}",
+            };
+        }
+    }
+}
